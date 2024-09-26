7a9fa07aa55d2d81bf5961f6dede15e78be42213
MarS-27 <marchuk1992@gmail.com>
2024-09-26 18:09:58 +0300

    created contacts page basic structure

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,5 +1,32 @@
+import clsx from 'clsx';
+import { PageTitle } from '../components/ui/PageTitle';
+import { useTranslation } from 'react-i18next';
+import { useGetContacts } from '../hooks/useGetContacts';
+import { PhoneNumber } from '../components/ui/PhoneNumber';
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const { t } = useTranslation();
+  const { contactsData } = useGetContacts();
+
+  return (
+    <section className={clsx('flex flex-col gap-5', 'max-md:gap-2.5')}>
+      <PageTitle divider={true}>{t('contactsPageTitle')}</PageTitle>
+      <div>
+        <p className="font-semibold">Адреса:</p>
+        {contactsData.addresses.map((a) => (
+          <p key={a.id}>{a.value}</p>
+        ))}
+      </div>
+      <div>
+        <p className="font-semibold">Режим роботи:</p>
+        <p>{contactsData.work_mode}</p>
+      </div>
+      <div>
+        <p className="font-semibold">Телефон для замовлення:</p>
+        <PhoneNumber />
+      </div>
+    </section>
+  );
 };
 
 export default Contacts;
