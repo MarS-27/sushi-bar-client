c51a85d7cbeaf9ea7f77cd888859a5f6501889a0
MarS-27 <marchuk1992@gmail.com>
2024-12-07 15:15:18 +0200

    changed banner pagination and navigation styles

--- src/components/home/BannersCarousel.tsx
+++ src/components/home/BannersCarousel.tsx
@@ -1,12 +1,35 @@
 import { useGetBanners } from '../../hooks/useGetBanners';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 export const BannersCarousel = () => {
   const { bannersData } = useGetBanners();
   console.log(bannersData);
 
   return (
-    <div>
-      <p>BannersCarousel</p>
-    </div>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className="custom-swiper"
+    >
+      {bannersData.map(({ attributes, id }) => (
+        <SwiperSlide key={id}>
+          <img
+            className="w-full rounded-md shadow-md"
+            src={attributes.img.data.attributes.url}
+            alt={attributes.img.data.attributes.name}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
